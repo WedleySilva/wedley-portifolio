4e01dc17ad99898853f7992620f644a80b57da47
WedleySilva <WedleySilva2105@gmail.com>
2024-08-22 20:25:25 -0300

    Teste de criação dos componentes

---
+++ src/components/FooterComp.jsx
@@ -0,0 +1,8 @@
+import '../style/FooterComp.css';
+
+function Footer() {
+    return <h1>Footer</h1>;
+  }
+  
+  export default Footer;
+  
---
+++ src/components/HeaderComp.jsx
@@ -0,0 +1,8 @@
+import '../style/HeaderComp.css';
+
+function Header() {
+    return <h1>Header</h1>;
+  }
+  
+  export default Header;
+  
