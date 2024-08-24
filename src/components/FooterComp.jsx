5b9dd4b231feeb5eb99d4a029428601fc9a92adc
WedleySilva <WedleySilva2105@gmail.com>
2024-08-23 21:25:00 -0300

    Footer concluído

--- src/components/FooterComp.jsx
+++ src/components/FooterComp.jsx
@@ -1,7 +1,30 @@
 import '../style/FooterComp.css';
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 const Footer = () => (
- <span>footer</span>
+ <footer>
+   <h1 className="h1Footer">
+      Wedley
+    </h1>
+    <div className="textFooter">
+      Full Stack Developer
+    </div>
+    <div className="iconsFooter">
+      <a href="https://github.com/wedleysilva" className="githubIcon" target="_blank" rel="noopener noreferrer">
+        <FaGithub />
+      </a>
+      <a href="https://www.linkedin.com/in/wedley-silva-schmoeller-809104247/" className="linkedinIcon" target="_blank" rel="noopener noreferrer">
+        <FaLinkedin />
+      </a>
+      <a href="mailto:wedleysilva21052gmail.com" className="mailIcon">
+        <MdEmail />
+      </a>
+    </div>
+    <div className="text2Footer">
+    © Wedley S. Schmoeller. <br />Todos os direitos reservados.
+    </div>
+ </footer>
 );
   
   export default Footer;
