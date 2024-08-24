dde58d2b3aabd9735ae4b980a5b78e15acca4888
WedleySilva <WedleySilva2105@gmail.com>
2024-08-24 00:06:13 -0300

    Componente MenuButton deletado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import MenuButton from "./components/MenuButton";
+
 import HeaderComp from "./components/HeaderComp";
 import MainComp from "./components/MainComp";
 import FooterComp from "./components/FooterComp";
@@ -8,7 +8,6 @@ import FooterComp from "./components/FooterComp";
 function App() {
   return (
     <Router>
-      <MenuButton />
       <HeaderComp />
       <MainComp />
       <FooterComp />
--- src/components/MenuButton.jsx
+++
@@ -1,9 +0,0 @@
-import "../style/MenuButton.css";
-
-const MenuButton = () => (
-  <div className="menuButton">
-        <button className="buttonMenu">Opção</button>
-  </div>
-);
-
-export default MenuButton;
