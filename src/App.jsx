9517edac4f783422198ccfb536545577cd8ac674
WedleySilva <WedleySilva2105@gmail.com>
2024-08-24 10:04:40 -0300

    Alterações em CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
-
 import HeaderComp from "./components/HeaderComp";
 import MainComp from "./components/MainComp";
 import FooterComp from "./components/FooterComp";
