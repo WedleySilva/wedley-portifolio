c5098c30ab752d671e993d217d2abc6f2965276d
WedleySilva <WedleySilva2105@gmail.com>
2024-08-23 21:27:22 -0300

    Importação dos componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import './App.css'
-import { BrowserRouter as Router } from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import MenuButton from "./components/MenuButton";
 import HeaderComp from "./components/HeaderComp";
+import MainComp from "./components/MainComp";
 import FooterComp from "./components/FooterComp";
-
 
 function App() {
   return (
     <Router>
+      <MenuButton />
       <HeaderComp />
+      <MainComp />
       <FooterComp />
     </Router>
   );
