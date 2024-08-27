bc4a4f7f260132a91239da8ae790d234e93148c4
WedleySilva <WedleySilva2105@gmail.com>
2024-08-27 20:38:34 -0300

    Ajuste header(versão Desktop)

--- src/components/HeaderComp.jsx
+++ src/components/HeaderComp.jsx
@@ -3,16 +3,9 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 
 const Header = () => (
-  <header>
-    <div className="text1Header">
-      Olá, eu sou
-    </div>
-    <h1 className="h1Header">
-      Wedley
-    </h1>
-    <div className="text2Header">
-      Full Stack Developer
-    </div>
+  <header className='headerComp'>
+
+    <div className="headerLeft">
     <div className="iconsHeader">
       <a href="https://github.com/wedleysilva" className="githubIcon" target="_blank" rel="noopener noreferrer">
         <FaGithub />
@@ -24,6 +17,25 @@ const Header = () => (
         <MdEmail />
       </a>
     </div>
+    </div>
+
+    <div className="headerCenter">
+    <div className="text1Header">
+      Olá, eu sou
+    </div>
+   
+    <h1 className="h1Header">
+      Wedley
+    </h1>
+    <div className="text2Header">
+      Full Stack Developer
+    </div>
+    </div>
+    
+   <div className="headerRight">
+
+   </div>
+   
   </header>
 );
 
