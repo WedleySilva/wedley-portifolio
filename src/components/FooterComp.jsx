660a7dcffcf9bbfad4b182702d2d274d7c21d655
WedleySilva <WedleySilva2105@gmail.com>
2024-08-27 20:38:16 -0300

    Ajuste footer(agora na versão de Desktop)

--- src/components/FooterComp.jsx
+++ src/components/FooterComp.jsx
@@ -1,31 +1,31 @@
-import '../style/FooterComp.css';
-import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { MdEmail } from "react-icons/md";
+  import '../style/FooterComp.css';
+  import { FaGithub, FaLinkedin } from "react-icons/fa";
+  import { MdEmail } from "react-icons/md";
 
-const Footer = () => (
- <footer>
-   <h1 className="h1Footer">
-      Wedley
-    </h1>
-    <div className="textFooter">
-      Full Stack Developer
-    </div>
-    <div className="iconsFooter">
-      <a href="https://github.com/wedleysilva" className="githubIcon" target="_blank" rel="noopener noreferrer">
-        <FaGithub />
-      </a>
-      <a href="https://www.linkedin.com/in/wedley-silva-schmoeller-809104247/" className="linkedinIcon" target="_blank" rel="noopener noreferrer">
-        <FaLinkedin />
-      </a>
-      <a href="mailto:wedleysilva21052gmail.com" className="mailIcon">
-        <MdEmail />
-      </a>
-    </div>
-    <div className="text2Footer">
-    © Wedley S. Schmoeller. <br />Todos os direitos reservados.
-    </div>
- </footer>
-);
-  
-  export default Footer;
-  
+  const Footer = () => (
+  <footer className='footerComp'>
+    <h1 className="h1Footer">
+        Wedley
+      </h1>
+      <div className="textFooter">
+        Full Stack Developer
+      </div>
+      <div className="iconsFooter">
+        <a href="https://github.com/wedleysilva" className="githubIcon" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/wedley-silva-schmoeller-809104247/" className="linkedinIcon" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="mailto:wedleysilva21052gmail.com" className="mailIcon">
+          <MdEmail />
+        </a>
+      </div>
+      <div className="text2Footer">
+      © Wedley S. Schmoeller. <br />Todos os direitos reservados.
+      </div>
+  </footer>
+  );
+    
+    export default Footer;
+    
