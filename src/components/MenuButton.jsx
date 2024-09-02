b9cf0d7fd4d0c96614d47cdca134d15036dcd93a
WedleySilva <WedleySilva2105@gmail.com>
2024-09-01 22:24:28 -0300

    Apagando botão

---
+++ src/components/MenuButton.jsx
@@ -0,0 +1,9 @@
+import "../style/MenuButton.css";
+
+const MenuButton = () => (
+  <div className="menuButton">
+    <button className="buttonMenu">Opção</button>
+  </div>
+);
+
+export default MenuButton;
