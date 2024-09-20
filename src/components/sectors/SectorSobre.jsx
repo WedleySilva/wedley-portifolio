71ed46a9ecc9faf6e8d255ad8b04263f61327743
WedleySilva <WedleySilva2105@gmail.com>
2024-09-19 23:37:56 -0300

    Foto sendo puxada da nuvem

--- src/components/sectors/SectorSobre.jsx
+++ src/components/sectors/SectorSobre.jsx
@@ -6,14 +6,14 @@ const SectorSobre = () => (
         Um pouco mais sobre mim
         </h2>
         <h3 className="textSobre">
-        Olá, eu me chamo <strong className='bold'>Wedley</strong>, tenho 19  anos de idade e <strong className='bold'>amo programação</strong>. 
+        Olá, meu nome é <strong className='bold'>Wedley Silva Schmoeller</strong>, tenho 19  anos de idade e <strong className='bold'>amo programação</strong>. 
         <div className="textSobre2">
         Sou apaixonado por <strong className='bold'>tecnologia</strong>, <strong className='bold'>softwares</strong> e <strong className='bold'>desenvolvimento</strong> de 
          soluções práticas, sendo assim programação é a minha área <strong className='bold'>profissional</strong> e meu <strong className='bold'>hobbie</strong>.
          </div>
         </h3>
         <div className='divImagemSobre'>
-            <img  className="imagemSectorSobre" src="https://images.pexels.com/photos/10692904/pexels-photo-10692904.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Wedley Silva Schmoeller" />
+            <img  className="imagemSectorSobre" src="https://firebasestorage.googleapis.com/v0/b/wedley-portifolio.appspot.com/o/Fotos%2FWedley_Schmoeller.png?alt=media&token=0a20e424-158d-41bb-8f4e-ac23d399770e" alt="Wedley Silva Schmoeller" />
         </div>
 
     </sector>
