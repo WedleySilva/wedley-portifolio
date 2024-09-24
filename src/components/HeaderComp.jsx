903c487159849ec57f10e4d91d928658226917c3
WedleySilva <WedleySilva2105@gmail.com>
2024-09-23 22:03:44 -0300

    Responsividade para Mobile no Header

--- src/components/HeaderComp.jsx
+++ src/components/HeaderComp.jsx
@@ -4,38 +4,45 @@ import { MdEmail } from "react-icons/md";
 
 const Header = () => (
   <header className='headerComp' id="home">
-
     <div className="headerLeft">
-    <div className="iconsHeader">
-      <a href="https://github.com/wedleysilva" className="githubIcon" target="_blank" rel="noopener noreferrer">
-        <FaGithub />
-      </a>
-      <a href="https://www.linkedin.com/in/wedley-silva-schmoeller-809104247/" className="linkedinIcon" target="_blank" rel="noopener noreferrer">
-        <FaLinkedin />
-      </a>
-      <a href="mailto:wedleysilva21052gmail.com" className="mailIcon">
-        <MdEmail />
-      </a>
-    </div>
+      <div className="iconsHeader">
+        <a href="https://github.com/wedleysilva" className="githubIcon" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/wedley-silva-schmoeller-809104247/" className="linkedinIcon" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="mailto:wedleysilva21052gmail.com" className="mailIcon">
+          <MdEmail />
+        </a>
+      </div>
     </div>
 
     <div className="headerCenter">
-    <div className="text1Header">
-      Olá, eu sou
-    </div>
-   
-    <h1 className="h1Header">
-      Wedley
-    </h1>
-    <div className="text2Header">
-      Full Stack Developer
-    </div>
-    </div>
-    
-   <div className="headerRight">
+      <div className="text1Header">
+        Olá, eu sou
+      </div>
+      <h1 className="h1Header">
+        Wedley
+      </h1>
+      <div className="text2Header">
+        Full Stack Developer
+      </div>
 
-   </div>
-   
+      <div className="iconsHeaderMobile">
+        <a href="https://github.com/wedleysilva" className="githubIconMobile" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/wedley-silva-schmoeller-809104247/" className="linkedinIconMobile" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="mailto:wedleysilva21052gmail.com" className="mailIconMobile">
+          <MdEmail />
+        </a>
+      </div>
+    </div>
+
+    <div className="headerRight"></div>
   </header>
 );
 
